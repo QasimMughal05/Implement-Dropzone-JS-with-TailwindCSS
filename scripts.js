faaa7ad3f363f014eb9857c0e2cb3afed3b5bdd1
Qasim Mughal <57962783+QasimMughal05@users.noreply.github.com>
2023-11-21 12:40:38 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
 // scripts.js
 
-Dropzone.options.myDropzone = {
-    init: function () {
-        this.on("success", function (file) {
-            // Add your success handling code here
-            file.previewTemplate.querySelector(".dz-success-mark").style.display = "block";
-        });
+ <script src="https://cdn.jsdelivr.net/npm/dropzone@5.9.2/dist/dropzone.js"></script>
+    <script>
+  var dropzone = new Dropzone('#demo-upload', {
+  previewTemplate: document.querySelector('#preview-template').innerHTML,
+  parallelUploads: 2,
+  thumbnailHeight: 120,
+  thumbnailWidth: 120,
+  maxFilesize: 3,
+  addRemoveLinks: true,
+  filesizeBase: 1000,
+  thumbnail: function(file, dataUrl) {
+    if (file.previewElement) {
+      file.previewElement.classList.remove("dz-file-preview");
+      var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+      for (var i = 0; i < images.length; i++) {
+        var thumbnailElement = images[i];
+        thumbnailElement.alt = file.name;
+        thumbnailElement.src = dataUrl;
+      }
+      setTimeout(function() { file.previewElement.classList.add("dz-image-preview"); }, 1);
+    }
+  }
 
-        this.on("removedfile", function (file) {
-            // Add your file removal code here
-            file.previewTemplate.querySelector(".dz-success-mark").style.display = "none";
-        });
-    },
-};
+});
